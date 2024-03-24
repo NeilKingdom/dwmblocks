4f2f1a85a9f3be41c19eef4a65d91e024de1da86
NeilKingdom <neil@neilkingdom.xyz>
2024-03-24 15:26:50 -0400

    Cleanup

--- Makefile
+++ Makefile
@@ -1,36 +1,33 @@
 PREFIX  := /usr/local
 CC      := cc
-CFLAGS  := -pedantic -Wall -Wno-deprecated-declarations -Os
+CCFLAGS := -pedantic -Wall -Wno-deprecated-declarations -Os
 LDFLAGS := -lX11
 
 # FreeBSD (uncomment)
 #LDFLAGS += -L/usr/local/lib -I/usr/local/include
-# # OpenBSD (uncomment)
+# OpenBSD (uncomment)
 #LDFLAGS += -L/usr/X11R6/lib -I/usr/X11R6/include
 
 all: options dwmblocks
 
 options:
 	@echo dwmblocks build options:
-	@echo "CFLAGS  = ${CFLAGS}"
-	@echo "LDFLAGS = ${LDFLAGS}"
-	@echo "CC      = ${CC}"
+	@echo "CCFLAGS = $(CCFLAGS)"
+	@echo "LDFLAGS = $(LDFLAGS)"
+	@echo "CC      = $(CC)"
 
-dwmblocks: dwmblocks.c config.def.h config.h
-	${CC} -o dwmblocks dwmblocks.c ${CFLAGS} ${LDFLAGS}
-
-config.h:
-	cp config.def.h $@
+dwmblocks: dwmblocks.c config.h
+	$(CC) -o dwmblocks dwmblocks.c $(CCFLAGS) $(LDFLAGS)
 
 clean:
-	rm -f *.o *.gch dwmblocks config.h
+	rm -f dwmblocks
 
 install: dwmblocks
-	mkdir -p ${DESTDIR}${PREFIX}/bin
-	cp -f dwmblocks ${DESTDIR}${PREFIX}/bin
-	chmod 755 ${DESTDIR}${PREFIX}/bin/dwmblocks
+	mkdir -p $(DESTDIR)$(PREFIX)/bin
+	cp -f dwmblocks $(DESTDIR)$(PREFIX)/bin
+	chmod 755 $(DESTDIR)$(PREFIX)/bin/dwmblocks
 
 uninstall:
-	rm -f ${DESTDIR}${PREFIX}/bin/dwmblocks
+	rm -f $(DESTDIR)$(PREFIX)/bin/dwmblocks
 
 .PHONY: all options clean install uninstall
--- config.h
+++ config.h
@@ -1,9 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
+#ifndef NO_X
+#include <X11/Xlib.h>
+#endif
+
+#ifdef __OpenBSD__
+#define SIGPLUS			(SIGUSR1 + 1)
+#define SIGMINUS		(SIGUSR1 - 1)
+#else
+#define SIGPLUS			SIGRTMIN
+#define SIGMINUS		SIGRTMIN
+#endif
+#define LENGTH(X)       (sizeof(X) / sizeof (X[0]))
+#define CMDLENGTH		50
+#define MIN(a, b) ((a < b) ? a : b)
+#define STATUSLENGTH (LENGTH(blocks) * CMDLENGTH + 1)
+
+typedef struct {
+	char* icon;
+	char* command;
+	unsigned int interval;
+	unsigned int signal;
+} Block;
+
+#ifndef __OpenBSD__
+void dummysighandler(int num);
+#endif
+void sighandler(int num);
+void getcmds(int time);
+void getsigcmds(unsigned int signal);
+void setupsignals();
+void sighandler(int signum);
+int getstatus(char *str, char *last);
+void statusloop();
+void termhandler();
+void pstdout();
+#ifndef NO_X
+void setroot();
+static void (*writestatus) () = setroot;
+static int setupX();
+static Display *dpy;
+static int screen;
+static Window root;
+#else
+static void (*writestatus) () = pstdout;
+#endif
+
+/*** User configuration ***/
+
 static const Block blocks[] = {
-	/* Icon          Command                                                      Update Interval   Update Signal */
-	{ " 💾 Mem: ",   "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		         0 },
-	{ " 📅 Date: ",  "date '+%b %d (%a)'",					                           60,		         0 },
-   /* NOTE: I've added intentional whitespace at the end of the date format for padding on the bar */
-   { " 🕛 Time: ",  "date '+%I:%M %p '",					                           60,	            0 },
+	/* Icon          Command                                                    Update Interval  Update Signal */
+    { " 💾 Mem: ",   "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		         0 },
+    { " 📅 Date: ",  "date '+%b %d (%a)'",					                    60,		         0 },
+    { " 🕛 Time: ",  "date '+%I:%M %p '",					                    60,	             0 },
 };
 
 /*
